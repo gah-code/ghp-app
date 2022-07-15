7aac8990cf3718f5e7c7d2a3e9424a1f037a24db
Gilbert Haro <gilbertharocode@gmail.com>
2022-07-15 02:01:00 -0700

    Form Netlify First Working Sub

--- src/components/Misc/Form.js
+++ src/components/Misc/Form.js
@@ -1,4 +1,4 @@
-import TestForm from './index';
+// import TestForm from './index';
 import './form.scss';
 
 //// TODO: Form button + Form-module-event + Form data
@@ -11,9 +11,15 @@ const Form = () => {
         <h3 class="heading-secondary"> Looking forward to hearing from you</h3>
       </div>
 
-      <TestForm />
+      {/* <TestForm /> */}
 
-      <form class="cta-form" name="contact" method="post">
+      <form
+        class="cta-form"
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit="submit"
+      >
 
         <div className="form-group">
           <div className="form-container">
