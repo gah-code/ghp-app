9844ac9d4f2acfbebb868cb03607f4c671e984a9
Gilbert Haro <gilbertharocode@gmail.com>
2022-07-15 00:09:27 -0700

    Form Netlify Test Five

--- src/components/Misc/Form.js
+++ src/components/Misc/Form.js
@@ -10,7 +10,13 @@ const Form = () => {
         <h3 class="heading-secondary"> Looking forward to hearing from you</h3>
       </div>
 
-      <form class="cta-form" name="contact" method="post" data-netlify="true">
+      <form
+        class="cta-form"
+        name="contact"
+        netlify
+        netlify-honeypot="bot-field"
+        hidden
+      >
         <div className="form-group">
           <div className="form-container">
             <label class="custom-field two">
