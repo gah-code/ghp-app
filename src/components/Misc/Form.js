0d700cb95768842a0d8b449165dae59834690734
Gilbert Haro <gilbertharocode@gmail.com>
2022-07-27 23:52:49 -0700

    Form Netlify 12

--- src/components/Misc/Form.js
+++ src/components/Misc/Form.js
@@ -39,26 +39,30 @@ const Form = () => {
       </div>
 
       <form
-        name='contact'
+        name='contact-form'
         method='post'
         data-netlify='true'
         onSubmit={handleSubmit}
       >
-        <input type='hidden' name='form-name' value='contact' />
+        <input type='hidden' name='form-name' value='contact-form' />
         <p>
           <label>
-            Your Name: <input type='text' name='name' onChange={handleChange} />
+            Your Name:
+            <br />
+            <input type='text' name='name' onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Your Email:{' '}
+            Your Email:
+            <br />
             <input type='email' name='email' onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Your Role:{' '}
+            Your Role:
+            <br />
             <select name='role[]' multiple>
               <option value='leader' onChange={handleChange}>
                 Leader
@@ -71,60 +75,14 @@ const Form = () => {
         </p>
         <p>
           <label>
-            Message:{' '}
+            Message:
+            <br />
             <textarea name='message' onChange={handleChange}></textarea>
           </label>
         </p>
         <p>
           <button type='submit'>Send</button>
         </p>
-
-        {/* <p>
-          <label className='form__label' htmlFor='email'>
-            Email Address
-          </label>
-          <input className='form__input' id='email' type='email' name='email' />
-          <ValidationError prefix='Email' field='email' errors={state.errors} />
-        </p>{' '}
-        <p>
-          <label className='form__label' htmlFor='fullname'>
-            Full name
-          </label>
-          <input
-            className='form__input'
-            id='fullname'
-            type='fullname'
-            name='fullname'
-          />
-          <ValidationError
-            prefix='Fullname'
-            field='fullname'
-            errors={state.errors}
-          />
-        </p>{' '}
-        <label className='form__label' for='department'>
-          Service
-        </label>
-        <select
-          className='form__input'
-          name='department'
-          id='department'
-          required=''
-        >
-          <option value='' selected='' disabled=''>
-            Select
-          </option>
-          <option value='Front Desk'>Front Desk</option>
-          <option value='Customer Care'>Customer Care</option>
-          <option value='Shipping Receiving'>Shipping / Receiving</option>
-          <option value='Billing Accounting'>Billing / Accounting</option>
-        </select> */}
-        {/* <textarea id='message' name='message' />
-        <ValidationError
-          prefix='Message'
-          field='message'
-          errors={state.errors}
-        /> */}
       </form>
     </section>
   );
