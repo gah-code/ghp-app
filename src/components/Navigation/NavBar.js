e0e6692441a20f91a1ba5d53d20f70b7e9f6c83a
Gilbert Haro <gilbertharocode@gmail.com>
2022-08-09 16:58:48 -0700

    Navigation onClick option

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -5,13 +5,21 @@ const NavBar = () => {
 
   return (
     <nav>
-      <div className="logo">
+      <div className='logo'>
         <h4> GahPhotography</h4>
       </div>
-      <ul className="nav-links">
-        <li><a onClick={foo} href="3" alt="">Services</a></li>
-        <li><a onClick={foo} href="3" alt="">About</a></li>
-        <li><a onClick={foo} href="3" alt="">Contact</a></li>
+      <ul className='nav-links'>
+        <li>
+          <a onClick={foo} href='3' alt=''>
+            Services
+          </a>
+        </li>
+        {/* <li><a onClick={foo} href="3" alt="">About</a></li> */}
+        <li>
+          <a onClick={foo} href='3' alt=''>
+            Contact
+          </a>
+        </li>
       </ul>
     </nav>
   );
